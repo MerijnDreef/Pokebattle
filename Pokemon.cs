3e54ea81ff6dbc0b8f13ba3ff89fc7fbe4ceb0c2
Merijn <99054535@mydavinci.nl>
2021-10-13 12:19:04 +0200

    thought I was done (I wasn't) I need to fix what is left now
    welp I have a lot to do, such as a get for the protected stuff and some other stuff

--- Pokemon.cs
+++ Pokemon.cs
@@ -1,25 +1,25 @@
 using System.Collections.Generic;
 namespace Pokebattle
 {
-    public class pokemon 
+    public class Pokemon 
     {
         public string Name { get; private set; }
         public Energytype Type { get; private set; }
         private int HitPoints { get; set; }
         public int Health { get; private set; }
-        internal List<Attack> Attacks { get; set; }
-        internal List<Resistance> Resistances { get; set; }
-        internal List<Weakness> Weaknesses { get; set; }
+        protected List<Attack> Attacks { get; set; }
+        protected Resistance Resistances { get; set; }
+        protected Weakness Weaknesses { get; set; }
 
-        public pokemon(string name, int hitpoints, Energytype energytype, List<Attack> attacks = null, List<Resistance> resistances = null, List<Weakness> weaknesses = null)
+        public Pokemon(string name, int hitpoints, Energytype energytype, List<Attack> attacks, Resistance resistances, Weakness weaknesses)
         {
             Name = name;
             HitPoints = hitpoints;
             Health = hitpoints;
             Type = energytype;
-            Attacks = attacks ?? new List<Attack>(); 
-            Resistances = resistances ?? new List<Resistance>();
-            Weaknesses = weaknesses ?? new List<Weakness>();
+            Attacks = attacks; 
+            Resistances = resistances;
+            Weaknesses = weaknesses;
         }
         /*
          * what it does first is, it looks if the weakness energytype is the same energytype as pokemon it's fighting
@@ -27,39 +27,40 @@ namespace Pokebattle
          * the last thing will just attack if the other ones didn't work
          *
          */
-        public void Damage(pokemon receiver, Energytype type, Attack attack) {
-            if (receiver.Type.TypeWeak == type.Type)
+        public void Damage(Pokemon dealer, int attack) {
+            if (Type.TypeWeak == dealer.Type.Type)
             {
-                receiver.Health -= (int)(attack.Damage * receiver.Weaknesses[0].Multiplier);
-            } else if(receiver.Type.TypeResist == type.Type)
+                Health -= (int)(dealer.Attacks[attack].Damage * Weaknesses.Multiplier);
+            } else if(Type.TypeResist == Type.Type)
             {
-                receiver.Health -= (attack.Damage - receiver.Resistances[0].Resisting);
+                Health -= (dealer.Attacks[attack].Damage - Resistances.Resisting);
             } else
             {
-                receiver.Health -= (attack.Damage);
+                Health -= (dealer.Attacks[attack].Damage);
             }
+        }
+
+        public void getAttack()
+        {
+
         }
     }
 
-    public class Pikachu : pokemon
+    public class Pikachu : Pokemon
     {
-        public Pikachu():base("Pikachu", 60, new Energytype("Lightning", "Fighting", "Fire"))
+        public Pikachu():base("Pikachu", 60, new Energytype("Lightning", "Fighting", "Fire"), new Resistance(Type, 20), new Weakness(Type, 1.5F))
         {
             Attacks.Add(new Attack("Electric Ring", 50));
             Attacks.Add(new Attack("Pika Punch", 20));
-            Resistances.Add(new Resistance(Type, 20));
-            Weaknesses.Add(new Weakness(Type, 1.5F));
         }
     }
 
-    public class Charmeleon : pokemon
+    public class Charmeleon : Pokemon
     {
-        public Charmeleon():base("Charmeleon", 60, new Energytype("Fire", "Lightning", "Water"))
+        public Charmeleon():base("Charmeleon", 60, new Energytype("Fire", "Lightning", "Water"), new Resistance(Type.TypeResist, 10), new Weakness(Type.TypeWeak, 2.0F))
         {
             Attacks.Add(new Attack("Head Butt", 10));
             Attacks.Add(new Attack("Flare", 30));
-            Resistances.Add(new Resistance(Type, 10));
-            Weaknesses.Add(new Weakness(Type, 2.0F));
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -17,10 +17,9 @@ namespace Pokebattle
 
             Console.WriteLine("press the Enter key to start the fight");
             Console.ReadLine();
-            Console.WriteLine($"{pokemon.Name}, {pokemon.Health}, {pokemon.Type.Type}, {pokemon.Attacks[0].Name}, {pokemon.Attacks[1].Name}, {pokemon.Type.TypeResist}, {pokemon.Resistances[0].Resisting}, {pokemon.Type.TypeWeak}, {pokemon.Weaknesses[0].Multiplier}");
-            
-            pokemon.Damage(pokemonHostile, pokemon.Type, pokemon.Attacks[0]);
-            pokemon.Damage(pokemon, pokemonHostile.Type, pokemonHostile.Attacks[1]);
+            //Console.WriteLine($"{pokemon.Name}, {pokemon.Health}, {pokemon.Type.Type}, {pokemon.Attacks[0].Name}, {pokemon.Attacks[1].Name}, {pokemon.Type.TypeResist}, {pokemon.Resistances[0].Resisting}, {pokemon.Type.TypeWeak}, {pokemon.Weaknesses[0].Multiplier}");
+            pokemonHostile.Damage(pokemon, 0);
+            pokemon.Damage(pokemonHostile, 1);
 
             Console.WriteLine($"Pikachu used {pokemon.Attacks[0].Name} on {pokemonHostile.Name} and lowered it's health to {pokemonHostile.Health}");
             Console.WriteLine($"Charmeleon used {pokemonHostile.Attacks[1].Name} on {pokemon.Name} and lowered it's health to {pokemon.Health}");
@@ -30,8 +29,8 @@ namespace Pokebattle
                 Console.WriteLine($"{pok.Name}");
             }
 
-            pokemon.Damage(pokemonHostile, pokemon.Type, pokemon.Attacks[1]);
-            pokemon.Damage(pokemon, pokemonHostile.Type, pokemonHostile.Attacks[0]);
+            pokemonHostile.Damage(pokemon, 1);
+            pokemon.Damage(pokemonHostile, 0);
 
             Console.WriteLine($"Pikachu used {pokemon.Attacks[1].Name} on {pokemonHostile.Name} and lowered it's health to {pokemonHostile.Health}");
             Console.WriteLine($"Charmeleon used {pokemonHostile.Attacks[0].Name} on {pokemon.Name} and lowered it's health to {pokemon.Health}");
@@ -40,14 +39,15 @@ namespace Pokebattle
             {
                 Console.WriteLine($"{pok.Name}");
             }
+
             Console.ReadKey();
         }
 
         // GetLivePokemon will check if the pokemons health isn't equal to 0, and if that is the case it will continue
-        static List<pokemon> GetLivePokemon() => Pokemons.Where(x => x.Health > 0).ToList();
+        static List<Pokemon> GetLivePokemon() => Pokemons.Where(x => x.Health > 0).ToList();
 
         // this puts the pokemon in a easy to acces list
-        static List<pokemon> Pokemons = new List<pokemon>();
+        static List<Pokemon> Pokemons = new List<Pokemon>();
 
     }
 }
